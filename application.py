ec670ab164425bdd8bc1c1ffbd8d21d5d4cbc620
JhosefMatheus <jhosef.mat123@hotmail.com>
2021-05-17 23:30:11 -0300

    fazendo alterações no estilo do app, adicionando classee converter.py responsável por fazer todas as conversões e cálculos do app

--- application.py
+++ application.py
@@ -2,10 +2,20 @@ from tkinter import *
 
 window = Tk()
 
-window.geometry("650x300")
+w = 650
+h = 300
+
+w_screen = window.winfo_screenwidth()
+h_screen = window.winfo_screenheight()
+
+position_x = (w_screen/2) - (w/2)
+position_y = (h_screen/2) - (h/2)
+
+window.geometry(f'{w}x{h}+{int(position_x)}+{int(position_y)}')
 window.configure(
     background='gray'
 )
+window.resizable(False, False)
 
 options_drop_down_1 = [
     'Armazenamento de Dados',
@@ -32,6 +42,87 @@ drop_down_1 = OptionMenu(
     *options_drop_down_1
 )
 
+options_drop_down_2 = [
+    'Quilômetro',
+    'Metro',
+    'Centímetro',
+    'Milímetro',
+    'Micrômetro',
+    'Nanômetro',
+    'Milha',
+    'Jarda',
+    'Pé',
+    'Polegada',
+    'Milha náutica'
+]
+default_drop_down_2 = options_drop_down_2[1]
+stringVar_drop_down_2 = StringVar(window)
+stringVar_drop_down_2.set(default_drop_down_2)
+drop_down_2 = OptionMenu(
+    window,
+    stringVar_drop_down_2,
+    *options_drop_down_2
+)
+
+options_drop_down_3 = [
+    'Quilômetro',
+    'Metro',
+    'Centímetro',
+    'Milímetro',
+    'Micrômetro',
+    'Nanômetro',
+    'Milha',
+    'Jarda',
+    'Pé',
+    'Polegada',
+    'Milha náutica'
+]
+default_drop_down_3 = options_drop_down_3[1]
+stringVar_drop_down_3 = StringVar(window)
+stringVar_drop_down_3.set(default_drop_down_3)
+drop_down_3 = OptionMenu(
+    window,
+    stringVar_drop_down_3,
+    *options_drop_down_3
+)
+
+entry = Entry(
+    window,
+    width=19,
+    font='Arial 19'
+)
+
+label_result = Label(
+    window,
+    font='Arial 18',
+    width=19
+)
+
+label_equal = Label(
+    window,
+    text='=',
+    font='Arial 20',
+    width=5,
+    bg='gray'
+)
+
+drop_down_1['width'] = w
+drop_down_1['font'] = 'Arial 20'
+drop_down_1['anchor'] = 'w'
+
+drop_down_2['width'] = 15
+drop_down_2['font'] = 'Arial 20'
+drop_down_2['anchor'] = 'w'
+
+drop_down_3['width'] = 15
+drop_down_3['font'] = 'Arial 20'
+drop_down_3['anchor'] = 'w'
+
 drop_down_1.pack()
+drop_down_2.place(x=7, y=150)
+drop_down_3.place(x=375, y=150)
+entry.place(x=7, y=110)
+label_result.place(x=375, y=110)
+label_equal.place(x=283, y=153)
 
 window.mainloop()
---
+++ converter.py
@@ -0,0 +1,53 @@
+class Converter:
+    def __init__(self):
+        pass
+
+    def change_option_menu(self, stringVar, option_menu_1, option_menu_2):
+        if stringVar == 'Armazenamento de Dados':
+            options_1 = [
+                'Bit',
+                'Kilobit',
+                'Kibibit',
+                'Megabit',
+                'Megbibit',
+                'Gigabit',
+                'Gibibit',
+                'Terabit',
+                'Tebibit',
+                'Petabit',
+                'Pebibit',
+                'Byte',
+                'Kilobyte',
+                'Kibibyte',
+                'Megabyte',
+                'MebiByte',
+                'Gigabyte',
+                'Gibibyte',
+                'Terabyte',
+            ]
+        elif stringVar == 'Comprimento':
+            pass
+        elif stringVar == 'Consumo de Energia':
+            pass
+        elif stringVar == 'Energia Mecânica':
+            pass
+        elif stringVar == 'Frequência':
+            pass
+        elif stringVar == 'Massa':
+            pass
+        elif stringVar == 'Pressão':
+            pass
+        elif stringVar == 'Temperatura':
+            pass
+        elif stringVar == 'Tempo':
+            pass
+        elif stringVar == 'Transmissão de dados':
+            pass
+        elif stringVar == 'Velocidade':
+            pass
+        elif stringVar == 'Volume':
+            pass
+        elif stringVar == 'Área':
+            pass
+        elif stringVar == 'Ângulo':
+            pass
